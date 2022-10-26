1d5e0959ce415b7181d365bf2a1f392301cfc97f
Edozie <edoziechukwuma@gmail.com>
2022-10-25 19:05:39 -0600

    Modify components

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -2,13 +2,23 @@ import { Button } from 'bootstrap';
 import React, { Component } from 'react';
 
 class Calculator extends Component {
+  state = {
+    count: 0
+  };
   render() { 
     return (
       <React.Fragment>
-        <h1>Hello Julian</h1>
-        <input type={'number'}/>
+        <h1>Calculator</h1>
+        <section className='container-lg'>
+          <input type={"input"} value={this.formatCount()} className='mh-100' />
+        </section>
       </React.Fragment>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state
+    return count === 0 ? 'Zero' : count;
   }
 }
  
