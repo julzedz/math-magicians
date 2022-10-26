d0c69c3f9afe4462bf644d7684d62c5353583383
Edozie <edoziechukwuma@gmail.com>
2022-10-25 18:18:41 -0600

    Modify Components

---
+++ src/components/calculator.jsx
@@ -0,0 +1,15 @@
+import { Button } from 'bootstrap';
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+  render() { 
+    return (
+      <React.Fragment>
+        <h1>Hello Julian</h1>
+        <input type={'number'}/>
+      </React.Fragment>
+    );
+  }
+}
+ 
+export default Calculator;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import Calculator from './components/calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Calculator />
   </React.StrictMode>,
 );
 
