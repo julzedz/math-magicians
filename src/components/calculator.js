b95013dfc9f915ab12dc51b2b063417acc401b52
Edozie <edoziechukwuma@gmail.com>
2022-10-29 10:05:15 -0600

    Refactor component to stateless

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,153 +1,147 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 import './calculator.css';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
 
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-  }
-
-  handleClick = (e) => {
+  const handleClick = (e) => {
     const element = e.target;
     const buttonName = element.value;
-    const result = calculate(this.state, buttonName);
-    this.setState(result);
-  }
+    const result = calculate(state, buttonName);
+    setState(result);
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <>
-        <section className="grid-container style">
-          <input
-            type="input"
-            value={(total || '') + (operation || '') + (next || '') || 0}
-            className="w-100 bg-secondary item1"
-          />
-          <input
-            type="button"
-            onClick={this.handleClick}
-            className="input"
-            value="AC"
-          />
-          <input
-            type="button"
-            onClick={this.handleClick}
-            className="input"
-            value="+/-"
-          />
-          <input
-            type="button"
-            onClick={this.handleClick}
-            className="input"
-            value="%"
-          />
-          <input
-            type="button"
-            onClick={this.handleClick}
-            className="orange"
-            value="÷"
-          />
-          <input
-            type="button"
-            onClick={this.handleClick}
-            className="input"
-            value="7"
-          />
-          <input
-            type="button"
-            onClick={this.handleClick}
-            className="input"
-            value="8"
-          />
-          <input
-            type="button"
-            onClick={this.handleClick}
-            className="input"
-            value="9"
-          />
-          <input
-            type="button"
-            onClick={this.handleClick}
-            className="orange"
-            value="x"
-          />
-          <input
-            type="button"
-            onClick={this.handleClick}
-            className="input"
-            value="4"
-          />
-          <input
-            type="button"
-            onClick={this.handleClick}
-            className="input"
-            value="5"
-          />
-          <input
-            type="button"
-            onClick={this.handleClick}
-            className="input"
-            value="6"
-          />
-          <input
-            type="button"
-            onClick={this.handleClick}
-            className="orange"
-            value="-"
-          />
-          <input
-            type="button"
-            onClick={this.handleClick}
-            className="input"
-            value="1"
-          />
-          <input
-            type="button"
-            onClick={this.handleClick}
-            className="input"
-            value="2"
-          />
-          <input
-            type="button"
-            onClick={this.handleClick}
-            className="input"
-            value="3"
-          />
-          <input
-            type="button"
-            onClick={this.handleClick}
-            className="orange"
-            value="+"
-          />
-          <input
-            type="button"
-            onClick={this.handleClick}
-            className="item0"
-            value="0"
-          />
-          <input
-            type="button"
-            onClick={this.handleClick}
-            className="input"
-            value="."
-          />
-          <input
-            type="button"
-            onClick={this.handleClick}
-            className="orange"
-            value="="
-          />
-        </section>
-      </>
-    );
-  }
-}
+  const { total, next, operation } = state;
+  return (
+    <>
+      <section className="grid-container style">
+        <input
+          type="input"
+          value={(total || '') + (operation || '') + (next || '') || 0}
+          className="w-100 bg-secondary item1"
+        />
+        <input
+          type="button"
+          onClick={handleClick}
+          className="input"
+          value="AC"
+        />
+        <input
+          type="button"
+          onClick={handleClick}
+          className="input"
+          value="+/-"
+        />
+        <input
+          type="button"
+          onClick={handleClick}
+          className="input"
+          value="%"
+        />
+        <input
+          type="button"
+          onClick={handleClick}
+          className="orange"
+          value="÷"
+        />
+        <input
+          type="button"
+          onClick={handleClick}
+          className="input"
+          value="7"
+        />
+        <input
+          type="button"
+          onClick={handleClick}
+          className="input"
+          value="8"
+        />
+        <input
+          type="button"
+          onClick={handleClick}
+          className="input"
+          value="9"
+        />
+        <input
+          type="button"
+          onClick={handleClick}
+          className="orange"
+          value="x"
+        />
+        <input
+          type="button"
+          onClick={handleClick}
+          className="input"
+          value="4"
+        />
+        <input
+          type="button"
+          onClick={handleClick}
+          className="input"
+          value="5"
+        />
+        <input
+          type="button"
+          onClick={handleClick}
+          className="input"
+          value="6"
+        />
+        <input
+          type="button"
+          onClick={handleClick}
+          className="orange"
+          value="-"
+        />
+        <input
+          type="button"
+          onClick={handleClick}
+          className="input"
+          value="1"
+        />
+        <input
+          type="button"
+          onClick={handleClick}
+          className="input"
+          value="2"
+        />
+        <input
+          type="button"
+          onClick={handleClick}
+          className="input"
+          value="3"
+        />
+        <input
+          type="button"
+          onClick={handleClick}
+          className="orange"
+          value="+"
+        />
+        <input
+          type="button"
+          onClick={handleClick}
+          className="item0"
+          value="0"
+        />
+        <input
+          type="button"
+          onClick={handleClick}
+          className="input"
+          value="."
+        />
+        <input
+          type="button"
+          onClick={handleClick}
+          className="orange"
+          value="="
+        />
+      </section>
+    </>
+  );
+};
 
 export default Calculator;
