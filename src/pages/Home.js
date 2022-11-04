2f7922c6ce00289a52d8e78437115e53c8b4d939
Edozie <edoziechukwuma@gmail.com>
2022-11-04 11:49:21 -0600

    Make corrections on Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,11 +7,11 @@ const Home = () => (
     <p>
       Mathematics is generally defined as the science that deals with numbers.
       It involves operations among numbers, and it also helps you to calculate
-      the product price, how many discounted prizes here, and If you good in
-      maths so you can calculate very fast. Mathematicians and scientists rely
-      on mathematics principles in their real-life to experiments with new
-      things every day. Many students say that ”I hate mathematics” and maths is
-      a useless subject, but it is wrong because without mathematics your life
+      the product price, discounted prizes, and if you are good in
+      maths, you can calculate very fast. Mathematicians and scientists rely
+      on mathematical principles in their real-life to experiment with new
+      things every day. Many students say ”I hate mathematics” and maths is
+      a pointless subject, but it is wrong because without mathematics your life
       is tough to survive. Math has its applications in every field.
     </p>
     <p>
